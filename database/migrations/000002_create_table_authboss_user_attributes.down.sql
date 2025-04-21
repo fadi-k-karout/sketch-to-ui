c10ea9e62f02d41673ea468e0843dc60e87e6e9d
fadi-k-karout <97478111+fadi-k-karout@users.noreply.github.com>
2025-04-21 19:01:21 +0300

    chore(migrations): delete old authboss migrations

--- database/migrations/000002_create_table_authboss_user_attributes.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE IF EXISTS authboss_user_attributes
--- database/migrations/000002_create_table_authboss_user_attributes.up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE authboss_user_attributes (
-    id SERIAL PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES authboss_user(id),
-    key VARCHAR(255) NOT NULL,
-    value TEXT NOT NULL,
-    UNIQUE (user_id, key)
-);
